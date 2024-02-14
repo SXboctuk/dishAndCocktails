588061ad1a77e4b4c97cd779d2f1f0377bf87962
Maxim <stargate.xboctuk@gmail.com@>
2024-02-14 15:03:26 +0300

    imports and env change

--- env.d.ts
+++ env.d.ts
@@ -10,6 +10,9 @@ interface ImportMetaEnv {
   readonly VITE_APP_FIREBASE_STORAGE_BUCKET: string
   readonly VITE_APP_FIREBASE_MESSAGING_SENDER_ID: string
   readonly VITE_APP_FIREBASE_APP_ID: string
+
+  readonly VITE_COCKTAIL_BASE_URL: string
+  readonly VITE_COCKTAIL_API_URL_SLUG: string
 }
 
 interface ImportMeta {
