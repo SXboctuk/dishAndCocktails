bd1a7d6d6c7bc5bfb195344fbb6c0fd1b6e3f87a
Maxim <stargate.xboctuk@gmail.com@>
2024-02-14 15:08:41 +0300

    build layout for cocktails

---
+++ src/components/cocktails/CocktailsLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <CocktailsHeader />
+  <div class="layout">
+    <main>
+      <UIContainer>
+        <RouterView />
+      </UIContainer>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CocktailsHeader from './CocktailsHeader.vue'
+import UIContainer from '@/components/ui/UIContainer.vue'
+</script>
+
+<style>
+.layout {
+  background-color: var(--cocktail-dark-1);
+  min-height: 100dvh;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
