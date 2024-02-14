19e78b0399fabcce71d3a43cd23041e9781b5462
Maxim <stargate.xboctuk@gmail.com@>
2024-02-14 22:00:09 +0300

    build routerName var

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,12 +7,26 @@ import AppLanding from '@/views/AppLanding.vue'
 import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+export const routerName = {
+  landing: 'landing',
+  meals: 'meals',
+  mealsHome: 'mealsHome',
+  cocktails: 'cocktails',
+  cocktailHome: 'cocktailHome',
+  cocktailDetails: 'cocktailDetails',
+  cocktailsCategories: 'cocktailsCategories',
+  cocktailsGlasses: 'cocktailsGlasses',
+  cocktailSearchByGlass: 'cocktailSearchByGlass',
+  cocktailsIngredients: 'cocktailsIngredients',
+  notFound: 'notFound'
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'landing',
+      name: routerName.landing,
       component: () =>
         defineAsyncComponent({
           loader: () => import('@/views/AppLanding.vue'),
@@ -22,7 +36,7 @@ const router = createRouter({
     },
     {
       path: '/meals',
-      name: 'meals',
+      name: routerName.meals,
       component: () =>
         defineAsyncComponent({
           loader: () => import('@/components/meals/MealsHeader.vue'),
@@ -32,6 +46,7 @@ const router = createRouter({
       children: [
         {
           path: '',
+          name: routerName.mealsHome,
           component: () =>
             defineAsyncComponent({
               loader: () => import('@/components/meals/MealsLanding.vue'),
@@ -43,7 +58,7 @@ const router = createRouter({
     },
     {
       path: '/cocktails',
-      name: 'cocktails',
+      name: routerName.cocktails,
       component: () =>
         defineAsyncComponent({
           loader: () => import('@/components/cocktails/CocktailsLayout.vue'),
@@ -53,7 +68,7 @@ const router = createRouter({
       children: [
         {
           path: '',
-          name: 'cocktailsHome',
+          name: routerName.cocktailHome,
           component: () =>
             defineAsyncComponent({
               loader: () =>
@@ -64,7 +79,7 @@ const router = createRouter({
         },
         {
           path: ':id',
-          name: 'cocktailsDetails',
+          name: routerName.cocktailDetails,
           component: () =>
             defineAsyncComponent({
               loader: () =>
@@ -72,6 +87,55 @@ const router = createRouter({
               errorComponent: AppErrorLoading,
               loadingComponent: CocktailsLoading
             })
+        },
+        {
+          path: 'ingredients',
+          name: routerName.cocktailsIngredients,
+          component: () =>
+            defineAsyncComponent({
+              loader: () =>
+                import('@/components/cocktails/CocktailsIngredients.vue'),
+              errorComponent: AppErrorLoading,
+              loadingComponent: CocktailsLoading
+            })
+        },
+        {
+          path: 'categories',
+          name: routerName.cocktailsCategories,
+          component: () =>
+            defineAsyncComponent({
+              loader: () =>
+                import('@/components/cocktails/CocktailsCategories.vue'),
+              errorComponent: AppErrorLoading,
+              loadingComponent: CocktailsLoading
+            })
+        },
+        {
+          path: 'glasses',
+          children: [
+            {
+              path: '',
+              name: routerName.cocktailsGlasses,
+              component: () =>
+                defineAsyncComponent({
+                  loader: () =>
+                    import('@/components/cocktails/CocktailsGlasses.vue'),
+                  errorComponent: AppErrorLoading,
+                  loadingComponent: CocktailsLoading
+                })
+            },
+            {
+              path: ':glass',
+              name: routerName.cocktailSearchByGlass,
+              component: () =>
+                defineAsyncComponent({
+                  loader: () =>
+                    import('@/components/cocktails/CocktailsSearchByGlass.vue'),
+                  errorComponent: AppErrorLoading,
+                  loadingComponent: CocktailsLoading
+                })
+            }
+          ]
         }
       ]
     }
