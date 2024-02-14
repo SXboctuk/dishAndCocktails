fbd5d924f34b3f2605376d7db331003e96a863f2
Maxim <stargate.xboctuk@gmail.com@>
2024-02-14 15:10:05 +0300

    build Details cocktail by ID

---
+++ src/components/cocktails/IngredientCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ingredient">
+    <div class="ingredient__image">
+      <AppImage :url="ingredienThumbURL(props.name)" />
+    </div>
+    <div class="ingredient__name cocktail-color__2 cocktail-font__2">
+      {{ props.name }}
+    </div>
+    <div
+      class="ingredient__measure cocktail-color__3 cocktail-font__3"
+      v-if="props.measure"
+    >
+      {{ props.measure }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useCocktails from '@/composables/useCocktails'
+import AppImage from '../AppImage.vue'
+
+const { ingredienThumbURL } = useCocktails()
+const props = defineProps<{
+  name: string
+  measure: string | null
+  size?: 'sm' | 'md' | 'lg'
+}>()
+</script>
+
+<style scoped lang="scss">
+.ingredient {
+  &__image {
+    aspect-ratio: 1/1;
+  }
+  &__name {
+    margin-top: 2rem;
+    line-height: 1;
+    text-align: center;
+  }
+  &__measure {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/stores/cocktails/details.ts
@@ -0,0 +1,40 @@
+import { computed, ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { NormalizedDrink } from '@/types/cocktails.types'
+import useCocktails from '@/composables/useCocktails'
+import useCocktalsNoormalizerData from '@/composables/useCocktailsNormalizerData'
+
+const useDetailsStore = defineStore('cocktails/details', () => {
+  const { normalizeDrink } = useCocktalsNoormalizerData()
+  const { lookupFullCocktailDetailsByID } = useCocktails()
+  const drinks = ref<Record<string, NormalizedDrink>>({})
+
+  function getDetails(id: string) {
+    const pending = ref<boolean>(false)
+    const error = ref<string | null>(null)
+    function fetch(id: string) {
+      error.value = null
+      pending.value = true
+
+      lookupFullCocktailDetailsByID(id)
+        .then((res) => {
+          const drinkRes = res.data.drinks[0]
+          if (drinkRes) {
+            drinks.value[id] = normalizeDrink(drinkRes)
+          } else {
+            throw new Error('Cocktail not found')
+          }
+        })
+        .catch((e: Error) => (error.value = e.message))
+        .finally(() => (pending.value = false))
+    }
+    if (!drinks.value[id]) {
+      fetch(id)
+    }
+    return { error, pending, details: computed(() => drinks.value[id]) }
+  }
+
+  return { getDetails }
+})
+
+export default useDetailsStore
