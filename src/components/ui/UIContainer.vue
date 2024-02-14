e8d6afa09a9f826509e2bbb3fb4c2c417bf44c3c
Maxim <stargate.xboctuk@gmail.com@>
2024-02-14 15:09:46 +0300

    build container ui

---
+++ src/components/ui/UIContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+<style scoped>
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
+<script setup lang="ts"></script>
