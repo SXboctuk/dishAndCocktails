b771e1e27081ad27cc4d7b7a4aa74a70c36118d3
Maxim <stargate.xboctuk@gmail.com@>
2024-02-21 00:09:42 +0300

    fix padding margin for scroll

--- src/components/ui/UIContainer.vue
+++ src/components/ui/UIContainer.vue
@@ -8,9 +8,11 @@
   display: flex;
   flex-direction: column;
   margin: 0 auto;
+
   max-width: 1280px;
   flex: 1 1 100%;
   padding: 0 1rem;
+  padding-right: 3.4rem;
 }
 </style>
 <script setup lang="ts"></script>
